9ed17c0711ee881eabc5aff13c74fe35891a1ba6
Hunter Clezie <clezhunt@gmail.com>
2026-03-14 09:23:22 -0600

    Add Header

--- DinerMenu/Form1.cs
+++ DinerMenu/Form1.cs
@@ -1,3 +1,10 @@
+/*
+Hunter Clezie 
+Spring 2026
+RCET2265
+DinerMenu
+github url: https://github.com/hunterclezie-pixel/DinerMenu.git
+*/
 namespace DinerMenu
 {
     public partial class Form1 : Form
